53e1f2b79a38eba9a4db0fb95f0699bd6a9824fe
TriangleDevv <chandu.lanka13@gmail.com>
2021-10-26 19:34:15 +0530

    coded a basic movement script for the player

--- main.lua
+++ main.lua
@@ -2,9 +2,25 @@ function love.load()
     player = {}
     player.x = 0
     player.y = 0
+    player.speed = 3
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("right") then
+        player.x = player.x + player.speed
+    end
+    
+    if love.keyboard.isDown("left") then
+        player.x = player.x - player.speed
+    end
+
+    if love.keyboard.isDown("down") then
+        player.y = player.y + player.speed
+    end
+
+    if love.keyboard.isDown("up") then
+        player.y = player.y - player.speed
+    end
 end
 
 function love.draw()
